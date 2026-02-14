2fc83fe9b9ffc27b99f9b228af219dbbf01474bc
spikechen <spikechen139@gmail.com>
2026-02-14 22:33:04 +0800

    Upload Lab 3 code

--- chatbot.py
+++ chatbot.py
@@ -3,10 +3,13 @@ This program requires the following modules:
 - python-telegram-bot==22.5
 - urllib3==2.6.2
 '''
+from logging import config
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import configparser
 import logging
+from ChatGPT_HKBU import ChatGPT
+gpt = None
 
 def main():
     # Configure logging so you can see initialization and error messages
@@ -17,6 +20,9 @@ def main():
     logging.info('INIT: Loading configuration...')
     config = configparser.ConfigParser()
     config.read('config.ini')
+
+    global gpt
+    gpt = ChatGPT(config)
 
     # Create an Application for your bot
     logging.info('INIT: Connecting the Telegram bot...')
@@ -31,11 +37,15 @@ def main():
     app.run_polling()
 
 async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await update.message.reply_text(response)
     logging.info("UPDATE: " + str(update))
+    loading_message = await update.message.reply_text('Thinking...')
 
-    # send the echo back to the client
-    text = update.message.text.upper()
-    await update.message.reply_text(text)
+    # send the user message to the ChatGPT client
+    response = gpt.submit(update.message.text)
+
+    # send the response to the Telegram box client
+    await loading_message.edit_text(response)
 
 if __name__ == '__main__':
     main()
